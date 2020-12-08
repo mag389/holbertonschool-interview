ae9124c8e22b867f8be72e34567b7fca222945b4
mag389 <mag389@cornell.edu>
2020-12-08 17:48:39 +0000

    first lockboxes problem

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""opening lockboxes module file"""
+
+
+def canUnlockAll(boxes):
+    """return true if all boxes can be unlocked, else false"""
+    locked = [1 for x in range(len(boxes))]
+    locked[0] = 0
+    locked = canUnlock(boxes, locked, 0)
+    if sum(locked) == 0:
+        return True
+    return False
+
+
+def canUnlock(boxes, locked, curIndex):
+    """determines lockednedd recursively, returns a list of locked boxes"""
+    locked[curIndex] = 0
+    for key in boxes[curIndex]:
+        if locked[key] == 1:
+            locked = canUnlock(boxes, locked, key)
+    return locked
