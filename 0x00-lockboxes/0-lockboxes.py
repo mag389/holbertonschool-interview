4e3fda2937f402eccfe71d42ab6284e3cdea0f8b
mag389 <mag389@cornell.edu>
2020-12-09 17:25:56 +0000

    accounting for keys without boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,6 +4,8 @@
 
 def canUnlockAll(boxes):
     """return true if all boxes can be unlocked, else false"""
+    if not boxes:
+        return False
     locked = [1 for x in range(len(boxes))]
     locked[0] = 0
     locked = canUnlock(boxes, locked, 0)
@@ -16,6 +18,8 @@ def canUnlock(boxes, locked, curIndex):
     """determines lockednedd recursively, returns a list of locked boxes"""
     locked[curIndex] = 0
     for key in boxes[curIndex]:
+        if key > len(boxes):
+            continue
         if locked[key] == 1:
             locked = canUnlock(boxes, locked, key)
     return locked
