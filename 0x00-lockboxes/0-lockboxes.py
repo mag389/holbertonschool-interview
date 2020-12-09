a3e8f5c261034b64c558a9e225920e64e146b17e
mag389 <mag389@cornell.edu>
2020-12-09 19:30:15 +0000

    non recursive function

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-"""opening lockboxes module file"""
+"""lockbox intervew question"""
 
 
 def canUnlockAll(boxes):
-    """return true if all boxes can be unlocked, else false"""
+    """
+    Method that determines if all the boxes can be opened.
+    """
     if not boxes:
         return False
-    return len(dfs(boxes, [])) == len(boxes)
 
-    # locked = [x for x in range(len(boxes))]
-    # locked[0] = 0
-    # print(locked)
-    # locked = canUnlock(boxes, locked, 0)
-    # print("--------")
-    # print(locked)
-    # if sum(locked) == 0:
-    #     return True
-    # return False
+    n = len(boxes)
+    keys = [0]
 
+    for key in keys:
+        for box in boxes[key]:
+            if box < n and box not in keys:
+                keys.append(box)
 
-def dfs(boxes, path, source=0):
-    path += [source]
-    for k in boxes[source]:
-        if k < len(boxes) and k not in set(path):
-            dfs(boxes, path, k)
-    return path
-
-
-def canUnlock(boxes, locked, curIndex):
-    """determines lockednedd recursively, returns a list of locked boxes"""
-    # print(locked)
-    locked[curIndex] = 0
-    for key in boxes[curIndex]:
-        if key < len(boxes) and locked[key] != 0:
-            locked = canUnlock(boxes, locked, key)
-    return locked
+    if len(keys) != n:
+        return False
+    return True
