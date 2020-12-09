f9f994293c8aedfed8bdb0d44e907a73e584044d
mag389 <mag389@cornell.edu>
2020-12-09 20:47:17 +0000

    last commit was wrong file

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""opening lockboxes module file"""
+"""lockbox intervew question"""
 
 
 def canUnlockAll(boxes):
-    """return true if all boxes can be unlocked, else false"""
+    """uses a queue to search for all boxes"""
     if not boxes:
         return False
-    locked = [x for x in range(len(boxes))]
-    locked[0] = 0
-    # print(locked)
-    locked = canUnlock(boxes, locked, 0)
-    # print("--------")
-    # print(locked)
-    if sum(locked) == 0:
-        return True
-    return False
 
+    n = len(boxes)
+    keys = [0]
 
-def canUnlock(boxes, locked, curIndex):
-    """determines lockednedd recursively, returns a list of locked boxes"""
-    # print(locked)
-    locked[curIndex] = 0
-    for key in boxes[curIndex]:
-        if key < len(boxes) and locked[key] != 0:
-            locked = canUnlock(boxes, locked, key)
-    return locked
+    for key in keys:
+        for box in boxes[key]:
+            if box < len(boxes) and box not in keys:
+                keys.append(box)
+
+    return len(keys) == len(boxes)
