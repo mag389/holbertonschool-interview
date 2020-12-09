96eb958f922dabfb6cd2a6582d3c211bdcae9bea
mag389 <mag389@cornell.edu>
2020-12-09 19:28:54 +0000

    slightly different method

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,15 +6,25 @@ def canUnlockAll(boxes):
     """return true if all boxes can be unlocked, else false"""
     if not boxes:
         return False
-    locked = [x for x in range(len(boxes))]
-    locked[0] = 0
+    return len(dfs(boxes, [])) == len(boxes)
+
+    # locked = [x for x in range(len(boxes))]
+    # locked[0] = 0
     # print(locked)
-    locked = canUnlock(boxes, locked, 0)
+    # locked = canUnlock(boxes, locked, 0)
     # print("--------")
     # print(locked)
-    if sum(locked) == 0:
-        return True
-    return False
+    # if sum(locked) == 0:
+    #     return True
+    # return False
+
+
+def dfs(boxes, path, source=0):
+    path += [source]
+    for k in boxes[source]:
+        if k < len(boxes) and k not in set(path):
+            dfs(boxes, path, k)
+    return path
 
 
 def canUnlock(boxes, locked, curIndex):
