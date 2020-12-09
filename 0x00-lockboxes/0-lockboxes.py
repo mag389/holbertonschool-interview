387e8886d2fab4da06ea3b11bc9fa392110fc9e7
mag389 <mag389@cornell.edu>
2020-12-09 20:58:24 +0000

    removed extra length line

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,13 +6,9 @@ def canUnlockAll(boxes):
     """uses a queue to search for all boxes"""
     if not boxes:
         return False
-
-    n = len(boxes)
     keys = [0]
-
     for key in keys:
         for box in boxes[key]:
             if box < len(boxes) and box not in keys:
                 keys.append(box)
-
     return len(keys) == len(boxes)
