b97b14fdae123bdf049d3df79c6768ceb5a0534d
mag389 <mag389@cornell.edu>
2021-01-19 19:07:08 +0000

    removed rubber duck prints

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -19,7 +19,6 @@ int is_palindrome(listint_t **head)
 	temp = *head;
 	n = is_palindrome_r(head, head);
 	head = &temp;
-	/* printf("n is %i \n", (*head)->n);*/
 	return (n);
 }
 
@@ -33,15 +32,12 @@ int is_palindrome_r(listint_t **head, listint_t **tail)
 {
 	int n;
 
-	/* printf("comparing %is to %i ===\n", (*head)->n, (*tail)->n);*/
 	if ((*tail)->next != NULL)
 	{
 		n = is_palindrome_r(head, &((*tail)->next));
-		/* printf("result is %i \n", n);*/
 		if (n == 0)
 			return (0);
 	}
-	/* printf("comparing %is to %i ===\n", (*head)->n, (*tail)->n);*/
 	if ((*head)->n == (*tail)->n)
 	{
 		*head = ((*head)->next);
