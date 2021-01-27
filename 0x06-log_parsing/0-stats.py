dca473d071d289a60d56336611f01dab78216f2a
mag389 <mag389@cornell.edu>
2021-01-27 23:44:45 +0000

    check for ragc differently

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -26,12 +26,12 @@ try:
         words = line.split()
         # print("after split")
         # print("{}".format(len(words)))
-        # if len(words) != 9:
-        #     raise Exception
-        filesize += int(words[-1])
-        code = int(words[-2])
-        coded[code] += 1
-        counter += 1
+        if len(words) >= 2:
+            #     raise Exception
+            filesize += int(words[-1])
+            code = int(words[-2])
+            coded[code] += 1
+            counter += 1
         if counter == 10:
             print("File size: {}".format(filesize))
             for num in range(len(codes)):
