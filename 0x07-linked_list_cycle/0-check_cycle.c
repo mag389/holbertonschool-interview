e0ce6734f91c743fba9904270ac9ea1c01a66ffb
mag389 <mag389@cornell.edu>
2021-02-01 18:44:38 +0000

    linked list cycle check function

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* check_cycle - checks for cycle in list
+* @list: pointer to head of list to check
+* Return: 0 if no cycle, else 1
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *tort, *hare;
+
+	tort = list;
+	hare = list->next;
+	while (hare)
+	{
+		if (tort == hare)
+			return (1);
+		tort = tort->next;
+		hare = hare->next;
+		if (hare)
+			hare = hare->next;
+		else
+			return (0);
+	}
+	return (0);
+}
