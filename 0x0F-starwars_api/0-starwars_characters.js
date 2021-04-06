a4af9931b36faa930ceab60935d94b69ba527c77
mag389 <mag389@cornell.edu>
2021-04-06 22:11:28 +0000

    file, readme and an enumerated list of the corret output

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const req = require('request');
+const film = process.argv[2];
+const theUrl = 'https://swapi-api.hbtn.io/api/films/';
+
+req.get({ url: theUrl + film }, async function (err, res, body) {
+  if (err) {
+    console.log('code: ' + res.statusCode);
+  } else {
+    const json = JSON.parse(body);
+    for (const urlc of json.characters) {
+      const code = await new Promise((resolve, reject) => {
+        req.get({ url: urlc }, async function getname (err1, res1, body1) {
+        if (err1) {
+          console.log('code: ' + res1.statusCode);
+        } else {
+          const json1 = await JSON.parse(body1);
+          // console.log(json1.name);
+          resolve(await json1.name);
+        }
+      })
+      });
+      // console.log(urlc);
+      console.log(code);
+    }
+  }
+});
---
+++ 0x0F-starwars_api/README.md
@@ -0,0 +1,19 @@
+# Star Wars (well the star wars api)
+
+This is about using a general api, uses the starwars api as an example
+
+required stuff:
+
+node 10:
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+semi-standard:
+sudo npm install semistandard --global
+
+request module:
+sudo npm install request --global
+export NODE_PATH=/usr/lib/node_modules
+
+
+extra source: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Concepts
