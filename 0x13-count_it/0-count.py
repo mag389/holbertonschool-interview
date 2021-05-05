70cef62e5bc0c8322ce8213bd71b41321ec0e947
mag389 <mag389@cornell.edu>
2021-05-05 22:51:47 +0000

    wrong title

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,88 +1,69 @@
 #!/usr/bin/python3
-""" script to scrape and count words from reddit hot posts
+"""
+    Reddit API
 """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """ uses reddit api to give top 10 hot posts
-        in a subreddit
-    """
-    custom_user = {"User-Agent": "custom"}
-    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
-    params = {'after': after}
-    res = requests.get(url,
-                       headers=custom_user, params=params,
-                       allow_redirects=False)
+import requests
 
-    if res.status_code != 200:
-        return None
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ gets the number of subscribers """
+    if after is not None:
+        url = 'https://api.reddit.com/r/' +\
+              '{}/hot.json?after={}'.format(subreddit, after)
     else:
-        info = res.json()
-        children = info.get('data').get('children')
-        for child in children:
-            hot_list.append(child.get('data').get("title"))
-        after = info.get('data').get('after')
-        if after == 'null' or after is None:
-            return (hot_list)
-        return (recurse(subreddit, hot_list, after))
+        url = 'https://api.reddit.com/r/{}/hot.json'.format(subreddit)
+    header = {'user-agent': 'ianscustomthing'}
 
-
-def count_words(subreddit, word_list, count_list=[]):
-    """  counts occurences of words in hot subreddit title list """
-    title_list = recurse(subreddit, [], "")
-    if type(title_list) is not list:
+    r = requests.get(url,
+                     headers=header,
+                     allow_redirects=False)
+    if r.status_code != 200:
+        return None
+    try:
+        rj = r.json()
+    except Exception:
+        return None
+    if rj.get('message') == 'Not Found':
         return
-    # title_list = " ".join(title_list).lower().split()
-    # word_dict = count_occurs(" ".join(title_list).lower(), word_list)
-    word_dict = count_occurs(title_list, word_list)
-    print_occurs(word_dict)
-
-
-def count_occurs(title_list, word_list, word_dict={}):
-    """ counts occurences, but only counts once per title """
-    word = word_list[-1].lower()
-    if word not in word_dict.keys():
-        word_dict[word] = 0
-    for title in title_list:
-        if word in title.lower().split():
-            word_dict[word] += 1
-    word_list.pop()
-    if len(word_list) < 1:
-        return word_dict
-    return count_occurs(title_list, word_list, word_dict)
-
-
-# this also counts mutiples more than once
-def icount_occurs(title_list, word_list, word_dict={}):
-    """ counts occurences of words in a list of titles """
-    word = word_list[-1].lower()
-    if word in word_dict.keys():
-        word_dict[word] += title_list.count(word)
+    chldrn = rj.get('data').get('children')
+    hot_list += [chld.get('data').get('title') for chld in chldrn]
+    after = rj.get('data').get('after')
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
     else:
-        word_dict[word] = title_list.count(word)
-    word_list.pop()
-    if len(word_list) < 1:
-        return word_dict
-    return icount_occurs(title_list, word_list, word_dict)
+        return hot_list
 
 
-def print_occurs(word_dict):
-    """ prints the dictionary sorted """
-    from operator import itemgetter
-    lst = sorted(word_dict.items(), key=itemgetter(1))
-    print_lst(lst)
-    return lst
+def print_words(word_list, hot_list):
+    """ prints the words all pretty like """
+    cnts = {}
+    for word in word_list:
+        c = 0
+        for title in hot_list:
+            t = title.lower().split()
+            if word.lower().strip() in t:
+                c += t.count(word.lower().strip())
+        if word.lower() in cnts:
+            cnts[word.lower()] += c
+        else:
+            cnts.update({word.lower(): c})
+    '''
+    print(hot_list)
+    cnts.update({'thing': 17})'''
+    for k, v in sorted(cnts.items(), key=lambda x: (-x[1], x[0])):
+        if cnts.get(k) != 0:
+            print("{}: {}".format(k, v))
 
 
-def print_lst(sorted_list):
-    """ print the list of words and numbers """
-    if len(sorted_list) < 1:
-        return None
-    tupword = sorted_list.pop()
-    if tupword[1] == 0:
-        print_lst(sorted_list)
-    else:
-        print(tupword[0] + ": ", end="")
-        print(tupword[1])
-        print_lst(sorted_list)
+def count_words(subreddit, word_list):
+    """ counts dem keywords """
+    hot_list = recurse(subreddit)
+    if hot_list is None:
+        return
+    f = 0
+    if f == 1:
+        count_words(subreddit, word_list)
+    print_words(word_list, hot_list)
