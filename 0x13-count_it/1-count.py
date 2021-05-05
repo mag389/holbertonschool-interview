6ed4fc152f1489e8c1c4c30a711d686154f2fff0
mag389 <mag389@cornell.edu>
2021-05-05 22:48:27 +0000

    trying alternate method for checker test

---
+++ 0x13-count_it/1-count.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+    Reddit API
+"""
+import requests
+
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ gets the number of subscribers """
+    if after is not None:
+        url = 'https://api.reddit.com/r/' +\
+              '{}/hot.json?after={}'.format(subreddit, after)
+    else:
+        url = 'https://api.reddit.com/r/{}/hot.json'.format(subreddit)
+    header = {'user-agent': 'ianscustomthing'}
+
+    r = requests.get(url,
+                     headers=header,
+                     allow_redirects=False)
+    if r.status_code != 200:
+        return None
+    try:
+        rj = r.json()
+    except Exception:
+        return None
+    if rj.get('message') == 'Not Found':
+        return
+    chldrn = rj.get('data').get('children')
+    hot_list += [chld.get('data').get('title') for chld in chldrn]
+    after = rj.get('data').get('after')
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
+
+
+def print_words(word_list, hot_list):
+    """ prints the words all pretty like """
+    cnts = {}
+    for word in word_list:
+        c = 0
+        for title in hot_list:
+            t = title.lower().split()
+            if word.lower().strip() in t:
+                c += t.count(word.lower().strip())
+        if word.lower() in cnts:
+            cnts[word.lower()] += c
+        else:
+            cnts.update({word.lower(): c})
+    '''
+    print(hot_list)
+    cnts.update({'thing': 17})'''
+    for k, v in sorted(cnts.items(), key=lambda x: (-x[1], x[0])):
+        if cnts.get(k) != 0:
+            print("{}: {}".format(k, v))
+
+
+def count_words(subreddit, word_list):
+    """ counts dem keywords """
+    hot_list = recurse(subreddit)
+    if hot_list is None:
+        return
+    f = 0
+    if f == 1:
+        count_words(subreddit, word_list)
+    print_words(word_list, hot_list)
