7100cc81563d14c3cbe6316439858d1a2c2a0327
mag389 <mag389@cornell.edu>
2021-06-02 16:32:34 +0000

    changed to reduce variables

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 listint_t *rec_loop(listint_t *head, listint_t *tort, listint_t *hare);
-listint_t *locate_loop(listint_t *head, listint_t *tort, listint_t *hare);
+/*listint_t *locate_loop(listint_t *head, listint_t *tort, listint_t *hare);*/
 /**
 * find_listint_loop - finds loop in linked list
 * Return: the loop node, or NULL
@@ -31,7 +31,16 @@ listint_t *rec_loop(listint_t *head, listint_t *tort, listint_t *hare)
 	{
 /*		printf("tort: %d, hare: %d\n", tort->n, hare->n);*/
 		if (hare == tort)
-			return (locate_loop(head, tort, hare));
+		{
+			while (hare)
+			{
+				if (head == hare)
+					return (head);
+				hare = hare->next;
+				head = head->next;
+			}
+		}
+/*			return (locate_loop(head, tort, hare));*/
 		if (!hare || !tort)
 			return (NULL);
 		tort = tort->next;
@@ -41,37 +50,43 @@ listint_t *rec_loop(listint_t *head, listint_t *tort, listint_t *hare)
 		else
 			return (NULL);
 	}
-	return (locate_loop(head, tort, hare));
+	return (hare);
 }
 /**
-* locate_loop - actually locates the linked list loop
+* locate_loop: actually locates the linked list loop
 * Return: the start of the loop
 * @head:the head node
 * @tort: a node known to be in the loop
 * @hare: a moving node stuck in the loop
 */
-listint_t *locate_loop(listint_t *head, listint_t *tort, listint_t *hare)
-{
+/*
+* prototype here locate loop
+* {
+*/
 /*
 *	printf("in locate loop\n");
 *	printf("initial tort: %d hare: %d \n", tort->n, hare->n);
 */
-	if (tort == hare)
-		hare = hare->next;
-	while (head != hare)
-	{
-		while (hare != tort)
-		{
-			if (head == hare)
-				return (head);
-			hare = hare->next;
+/*
+*	if (tort == hare)
+*		hare = hare->next;
+*	while (head != hare)
+*	{
+*		while (hare != tort)
+*		{
+*			if (head == hare)
+*				return (head);
+*			hare = hare->next;
+*/
 /*			printf("hare: %d going around\n", hare->n);*/
-		}
+/*		}*/
 /*		printf("moving head: %d \n", head->n);*/
-		head = head->next;
-		hare = hare->next;
-		if (head == hare || head == tort)
-			return (head);
-	}
-	return (head);
-}
+/*
+*		head = head->next;
+*		hare = hare->next;
+*		if (head == hare || head == tort)
+*			return (head);
+*	}
+*	return (head);
+*}
+*/
