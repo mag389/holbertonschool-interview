84fd3d926dfc6be830224db27517aa1c01ee3368
mag389 <mag389@cornell.edu>
2021-06-24 04:09:27 +0000

    large change comparison

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,46 +1,20 @@
 #!/usr/bin/python3
-""" making change """
-import numpy as np
+"""
+Making change file
+"""
 
 
 def makeChange(coins, total):
-    """ makes change for total amount from coin values in coins """
-    if type(total) is not int:
-        return 0
-    if type(coins) is not list or len(coins) <= 0:
-        return 0
     if total <= 0:
         return 0
-    if total in coins:
-        return 1
-    else:
-        # print("in else")
-        # minCoins = np.zeros(total + 1)
-        minCoins = [-1] * (total + 1)
-        minCoins[0] = 0
-        usedCoins = [-1] * (total + 1)
-        usedCoins[0] = 0
-        for cents in range(total + 1):
-            coincount = cents
-            countchanged = 0
-            newcoin = 0
-            for j in [c for c in coins if c <= cents]:
-                if (minCoins[cents - j] + 1 <= coincount and
-                        minCoins[cents - j] != -1):
-                    countchanged = 1
-                    coincount = minCoins[cents - j] + 1
-                    newcoin = j
-            if countchanged == 1:
-                minCoins[cents] = coincount
-                usedCoins[cents] = newcoin
-    # print(minCoins[total])
-    # print(usedCoins)
-    totally = minCoins[total]
-    totally = total
-    while totally > 0:
-        # print("used {}".format(totally))
-        if usedCoins[totally] < 0:
-            # print("return in while")
-            return -1
-        totally -= usedCoins[totally]
-    return minCoins[total]
+    goal = 0
+    usedCoins = 0
+
+    for c in reversed(sorted(coins)):
+        while goal + c <= total:
+            usedCoins += 1
+            goal += c
+
+    if goal != total:
+        return -1
+    return usedCoins
