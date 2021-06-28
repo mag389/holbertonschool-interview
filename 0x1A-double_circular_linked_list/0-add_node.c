beb8b7f7f8b165c41baec64b568d31118a1daa02
mag389 <mag389@cornell.edu>
2021-06-28 21:31:29 +0000

    tests well locally, add node to begining and end funcitons

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,60 @@
+#include "list.h"
+
+/**
+* add_node_end - adds new node at end
+* Return: the new node or NULL
+* @list: the head node of list to add to
+* @str: the string for new node
+*/
+List *add_node_end(List **list, char *str)
+{
+	List *new;
+
+	new = malloc(sizeof(List));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->next = new;
+	new->prev = new;
+
+	if (*list == NULL)
+		*list = new;
+	else
+	{
+		new->next = *list;
+		new->prev = (*list)->prev;
+		new->next->prev = new;
+		new->prev->next = new;
+	}
+	return (new);
+}
+
+/**
+* add_node_begin - adds new node at beginning
+* Return: NULL or the new node
+* @list: pointer to head of list to add to
+* @str: the new string to add to list
+*/
+List *add_node_begin(List **list, char *str)
+{
+	List *new;
+
+	new = malloc(sizeof(List));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->next = new;
+	new->prev = new;
+
+	if (*list)
+	{
+		new->next = *list;
+		new->prev = (*list)->prev;
+		(*list)->prev = new;
+		new->prev->next = new;
+	}
+	*list = new;
+	return (new);
+}
---
+++ 0x1A-double_circular_linked_list/README.md
@@ -0,0 +1,5 @@
+# Double circular linked list
+
+A double circular has both the propertie of a doubly linked list and a circular linked list.
+
+
