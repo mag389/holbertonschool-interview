d10f8f8f20641e8cc7487cba0555e24f7d8a720d
mag389 <mag389@cornell.edu>
2021-08-02 20:39:13 +0000

    pascal triangle method

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" pascal's triangle """
+
+
+def pascal_triangle(n):
+    """ makes a pascal triangle """
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    """ else"""
+    newlist = [1 for i in range(0, n)]
+    oldlist = pascal_triangle(n-1)
+    oldlist.append(newlist)
+    for i in range(1, n-1):
+        newlist[i] = oldlist[n - 2][i] + oldlist[n-2][i - 1]
+    return oldlist
