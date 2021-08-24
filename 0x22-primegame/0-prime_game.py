c0900b614e491759db6fc624be704d25c267f188
mag389 <mag389@cornell.edu>
2021-08-24 16:50:06 +0000

    prime game player funciton

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" prime game """
+
+
+def isWinner(x, nums):
+    """ determines the winner of the prime game
+        x: number of rounds
+        nums: array of n
+        Return: name of the winner
+            or None if undetermined
+        n and x both <= 10000
+    """
+    ben, maria = 0, 0
+    a = [1 for x in range(sorted(nums)[-1])]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        remove_multiples(a, i)
+    # print(a)
+    # print(nums)
+    for i in nums:
+        if sum(a[0:i]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def remove_multiples(listy, x):
+    """ removes multiples of a prime from a list
+        listy: list to remove from
+        x: the prime to remove
+    """
+    for i in range(2, len(listy)):
+        try:
+            listy[i * x] = 0
+        except (ValueError, IndexError):
+            break
